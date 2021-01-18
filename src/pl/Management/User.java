8702872cf5b1092fa305168c30fa2e018f921857
unknown <pawlowski.damian@hotmail.com>
2021-01-18 17:44:45 +0100

    Create User class

---
+++ src/pl/Management/User.java
@@ -0,0 +1,13 @@
+package pl.Management;
+
+public class User {
+    private String name;
+    private String lastName;
+    private Integer age;
+
+    public User(String name, String lastName, Integer age) {
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+    }
+}
