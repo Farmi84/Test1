a3eacdf74c1db042aaed0fd00233742668d80521
unknown <pawlowski.damian@hotmail.com>
2021-01-18 18:02:57 +0100

    Implement method calculate in main class

--- src/pl/Management/Main.java
+++ src/pl/Management/Main.java
@@ -1,8 +1,22 @@
 package pl.Management;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Podaj pierwszą liczbę");
+        int a = scanner.nextInt();
+        System.out.println("Podaj drugą liczbę");
+        int b = scanner.nextInt();
+        System.out.println("a + b = " + Calculator.add(a, b));
+        System.out.println("a - b = " + Calculator.subtract(a, b));
+        System.out.println("a * b = " + Calculator.multiply(a, b));
+        System.out.println("a / b = " + Calculator.divide(a, b));
+
     }
 }
